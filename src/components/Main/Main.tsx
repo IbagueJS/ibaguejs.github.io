100da9ae33047c7b9637de85426b5791b4af4f9f
davilapa <diegoavilapava@gmail.com>
2023-04-09 17:52:28 -0500

    feat: updating styles in Desktop Navbar to new design

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,58 +1,36 @@
 import { Title } from '../Title/Title';
 import { Navbar } from '../Navbar/Navbar';
-import { Button, Container, Input, useDisclosure } from '@chakra-ui/react'
 import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
+  extendTheme,
+  ChakraProvider,
+  Container,
 } from '@chakra-ui/react'
-import { useRef } from 'react';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  yellow: {
+    100: '#403900',
+    200: '#807300',
+    400: '#FFE400',
+    500: '#E6CF00'
+  }
+}
+
+const theme = extendTheme({ colors })
 
 export const Main = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef()
   return (
-    <main>
-      <Container centerContent paddingX='164px'>
-
-        <Navbar />
-        <Title className="text-gradient" />
-        <p className="instructions">
-          To get started, open the directory <code>src/pages</code> in your project.<br />
-          <strong>Code Challenge:</strong> Tweak the "Welcome to Astro" message above.
-        </p>
-        <Button colorScheme='blue'>Button</Button>
-        <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
-        Open
-      </Button>
-      <Drawer
-        isOpen={isOpen}
-        placement='right'
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Create your account</DrawerHeader>
-
-          <DrawerBody>
-            <Input placeholder='Type here...' />
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
-            </Button>
-            <Button colorScheme='blue'>Save</Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-      </Container>
-    </main>
+    <ChakraProvider theme={theme}>
+      <main>
+        <Container maxWidth="1352px" centerContent >
+          <Navbar />
+          <Title className="text-gradient" />
+          <p className="instructions">
+            To get started, open the directory <code>src/pages</code> in your project.<br />
+            <strong>Code Challenge:</strong> Tweak the "Welcome to Astro" message above.
+          </p>
+        </Container>
+      </main>
+    </ChakraProvider>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,28 @@
+import { Button } from '@chakra-ui/react'
 import './Navbar.scss'
 
 export const Navbar = () => {
   return (
     <nav className='navbar'>
-      <img className='navbar__icon' src="IBE.JS.svg" alt="" />
-      <ul className="section-links">
-        <li>
-          <a href="#who-we-are">Quiénes Somos</a>
-        </li>
-        <li>
-            <a href="#sponsors">Sponsors</a>
-        </li>
-        <li>
-            <a href="#events">Eventos</a>
-        </li>
-        <li>
-            <a href="#organizers">Organizadores</a>
-        </li>
-      </ul>
+      <img className='navbar__icon' src="ibaguejs_black_and_white.png" alt="" />
       <div className="navbar__right">
-        <div className="social-media">
-
-        </div>
+        <ul className="section-links">
+          <li>
+            <a className='active' href="#who-we-are">Quiénes Somos</a>
+          </li>
+          <li>
+              <a href="#sponsors">Sponsors</a>
+          </li>
+          <li>
+              <a href="#events">Eventos</a>
+          </li>
+          <li>
+              <a href="#organizers">Organizadores</a>
+          </li>
+        </ul>
+        <Button color='yellow.100' colorScheme='yellow'>
+          ser colaborador
+        </Button>
         <span className="menu material-icons">menu</span>
       </div>
     </nav>
