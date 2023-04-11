798fe5d05f51bba54af0f7b90b3bb6fd088d3b86
davilapa <diegoavilapava@gmail.com>
2023-04-10 21:04:26 -0500

    feat: adding hero page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,7 @@
-import { Title } from '../Title/Title';
-import { Navbar } from '../Navbar/Navbar';
-import {
-  extendTheme,
-  ChakraProvider,
-  Container,
-} from '@chakra-ui/react'
+import { Title } from '../Title/Title'
+import { Navbar } from '../Navbar/Navbar'
+import { extendTheme, ChakraProvider, Container } from '@chakra-ui/react'
+import { Hero } from '../Banner/Hero'
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
@@ -12,8 +9,8 @@ const colors = {
     100: '#403900',
     200: '#807300',
     400: '#FFE400',
-    500: '#E6CF00'
-  }
+    500: '#E6CF00',
+  },
 }
 
 const theme = extendTheme({ colors })
@@ -22,12 +19,16 @@ export const Main = () => {
   return (
     <ChakraProvider theme={theme}>
       <main>
-        <Container maxWidth="1352px" centerContent >
+        <Container maxWidth="1352px" centerContent>
           <Navbar />
+          <Hero />
           <Title className="text-gradient" />
           <p className="instructions">
-            To get started, open the directory <code>src/pages</code> in your project.<br />
-            <strong>Code Challenge:</strong> Tweak the "Welcome to Astro" message above.
+            To get started, open the directory <code>src/pages</code> in your
+            project.
+            <br />
+            <strong>Code Challenge:</strong> Tweak the "Welcome to Astro"
+            message above.
           </p>
         </Container>
       </main>
