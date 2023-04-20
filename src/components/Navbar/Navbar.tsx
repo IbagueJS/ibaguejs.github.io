509280a80549ad964586e564f9d1af86831c53ed
davilapa <diegoavilapava@gmail.com>
2023-04-20 18:38:52 -0500

    feat: adding google analytics

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,9 @@ import { Button, useBoolean } from '@chakra-ui/react'
 import './Navbar.scss'
 import { MenuButton } from '../MenuButton/MenuButton'
 import classNames from 'classnames'
-import { useState } from 'react'
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useBoolean()
-  const [hash, setHash] = useState(location.hash)
 
   return (
     <nav className={classNames('navbar', { 'is-open': isOpen })}>
@@ -14,40 +12,16 @@ export const Navbar = () => {
       <div className="navbar__right">
         <ul className="section-links">
           <li>
-            <a
-              className={classNames({ active: hash === 'who-we-are' ||  hash === ''})}
-              href="#who-we-are"
-              onClick={() => setHash('who-we-are')}
-            >
-              Quiénes Somos
-            </a>
+            <a href="#who-we-are">Quiénes Somos</a>
           </li>
           <li>
-            <a
-              className={classNames({ active: hash === 'sponsors' })}
-              href="#sponsors"
-              onClick={() => setHash('sponsors')}
-            >
-              Sponsors
-            </a>
+            <a href="#sponsors">Sponsors</a>
           </li>
           <li>
-            <a
-              className={classNames({ active: hash === 'events' })}
-              href="#events"
-              onClick={() => setHash('events')}
-            >
-              Eventos
-            </a>
+            <a href="#events">Eventos</a>
           </li>
           <li>
-            <a
-              className={classNames({ active: hash === 'organizers' })}
-              href="#organizers"
-              onClick={() => setHash('organizers')}
-            >
-              Organizadores
-            </a>
+            <a href="#organizers">Organizadores</a>
           </li>
         </ul>
         <Button
